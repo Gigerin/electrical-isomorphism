666ad605c09012d2779e7a1e8d7db7e5a9a997fb
Gigerin <codenamedelta228@gmail.com>
2024-04-15 22:54:45 +0300

    Reformat + edge colors

--- new_topology.py
+++ new_topology.py
@@ -5,16 +5,14 @@ import networkx as nx
 from util import *
 import matplotlib.patches as patches
 import matplotlib
-from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import matplotlib.image as image
-from dataclasses import fields, asdict
+from dataclasses import asdict
+
 matplotlib.use("TkAgg")
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-from shapely.geometry import Polygon, MultiPolygon
-
-
+from shapely.geometry import MultiPolygon
 
 
 TRANSISTOR_FILE_NAME = "static/png-clipart-transistor-npn-electronics-electronic-symbol-symbol-miscellaneous-electronics.png"
@@ -22,7 +20,8 @@ TRANSISTOR_FILE_NAME = "static/png-clipart-transistor-npn-electronics-electronic
 # showing the layers in matplotlib
 transistor_img = image.imread(TRANSISTOR_FILE_NAME)
 
-#TODO добавить повсеместно типизацию
+
+# TODO добавить повсеместно типизацию
 def two_comp_contains(comp1, comp2):
     """
     check if two components are intersected
@@ -30,13 +29,16 @@ def two_comp_contains(comp1, comp2):
     :param comp2:
     :return:
     """
-    comp1 = asdict(comp1).values() #TODO неэффективно каждый раз все в словарь переводить.
+    comp1 = asdict(
+        comp1
+    ).values()  # TODO неэффективно каждый раз все в словарь переводить.
     comp2 = asdict(comp2).values()
     for poly1 in comp1:
         for poly2 in comp2:
             if poly1.contains(poly2) or poly2.contains(poly1):
                 return True
     return False
+
 
 def two_comp_intersects(comp1, comp2):
     """
@@ -45,7 +47,9 @@ def two_comp_intersects(comp1, comp2):
     :param comp2:
     :return:
     """
-    comp1 = asdict(comp1).values() #TODO неэффективно каждый раз все в словарь переводить.
+    comp1 = asdict(
+        comp1
+    ).values()  # TODO неэффективно каждый раз все в словарь переводить.
     comp2 = asdict(comp2).values()
     for poly1 in comp1:
         for poly2 in comp2:
@@ -63,32 +67,41 @@ def convert_dict_to_graph(dict):
     graph = networkx.Graph()
     for key in dict.keys():
         collective = dict[key]
-        collective_dict = asdict(collective).values() #TODO ОЧЕРЕДНОЙ КОСТЫЛЬ БЛЯТЬ
+        collective_dict = asdict(collective).values()  # TODO ОЧЕРЕДНОЙ КОСТЫЛЬ БЛЯТЬ
         collective_poly = MultiPolygon(collective_dict)
         centroid = collective_poly.centroid
-        graph.add_node(key, pos = [centroid.x, centroid.y])
+        graph.add_node(key, pos=[centroid.x, centroid.y])
     for comp1 in dict.keys():
         for comp2 in dict.keys():
-            if comp1 == comp2: #чек если одна и та же компонента(петли не хотим)
+            if comp1 == comp2:  # чек если одна и та же компонента(петли не хотим)
                 continue
-            if 'contact' in comp1 or 'contact' in comp2:
-                if two_comp_contains(dict[comp1], dict[comp2]):#TODO некоторые пары мы проходим дважды, неэффективно
+            if "contact" in comp1 or "contact" in comp2:
+                if two_comp_contains(
+                    dict[comp1], dict[comp2]
+                ):  # TODO некоторые пары мы проходим дважды, неэффективно
                     graph.add_edge(comp1, comp2)
                     graph.add_edge(comp2, comp1)
-            if 'transistor' in comp1 and 'transistor' in comp2:
-                if two_comp_intersects(dict[comp1], dict[comp2]):#TODO некоторые пары мы проходим дважды, неэффективно
+            if "transistor" in comp1 and "transistor" in comp2:
+                if two_comp_intersects(
+                    dict[comp1], dict[comp2]
+                ):  # TODO некоторые пары мы проходим дважды, неэффективно
                     graph.add_edge(comp1, comp2)
                     graph.add_edge(comp2, comp1)
-            if 'transistor' in comp1 and 'SI_rail' in comp2:
-                if two_comp_intersects(dict[comp1], dict[comp2]):#TODO некоторые пары мы проходим дважды, неэффективно
+            if "transistor" in comp1 and "SI_rail" in comp2:
+                if two_comp_intersects(
+                    dict[comp1], dict[comp2]
+                ):  # TODO некоторые пары мы проходим дважды, неэффективно
                     graph.add_edge(comp1, comp2)
                     graph.add_edge(comp2, comp1)
-            if 'transistor' in comp1 and 'Eqwi' in comp2:
-                if two_comp_intersects(dict[comp1], dict[comp2]):#TODO некоторые пары мы проходим дважды, неэффективно
+            if "transistor" in comp1 and "Eqwi" in comp2:
+                if two_comp_intersects(
+                    dict[comp1], dict[comp2]
+                ):  # TODO некоторые пары мы проходим дважды, неэффективно
                     graph.add_edge(comp1, comp2)
                     graph.add_edge(comp2, comp1)
 
     return graph
+
 
 def draw_schema(data):
     num_keys = 10000
@@ -121,8 +134,6 @@ def draw_schema(data):
     plt.show()
 
 
-
-
 file_name = input("Please enter name of file(blank for default):")
 if not file_name:
     file_name = "sum.cif"
@@ -135,20 +146,47 @@ graph1 = convert_dict_to_graph(data)
 graph2 = convert_dict_to_graph(data)
 graph2.add_edge("r_contact114", "m_contact135")
 print(networkx.vf2pp_is_isomorphic(graph1, graph2))
-node_positions = {node: attributes['pos'] for node, attributes in graph1.nodes(data=True)}
+node_positions = {
+    node: attributes["pos"] for node, attributes in graph1.nodes(data=True)
+}
 
 # Use spring layout to automatically position nodes
 pos = nx.spring_layout(graph1, pos=node_positions, fixed=node_positions.keys(), k=1000)
 
 
-color_dict = {'SI': 'green', 'M1': 'yellow', 'M2': 'cyan', 'n_transistor': 'red', 'p_transistor': 'blue'}
+color_dict = {
+    "SI": "green",
+    "M2": "cyan",
+    "M1": "yellow",
+    "n_transistor": "red",
+    "p_transistor": "blue",
+}
 
-def find_color(node_name):
+
+def find_color_node(node_name):
     for substring, color in color_dict.items():
         if substring in str(node_name):
             return color
     # Default color if no match found
-    return 'black'
+    return "black"
 
-networkx.draw(graph1, pos, node_color=[find_color(node) for node in graph1.nodes()], with_labels = True)
+
+def find_color_edge(edge):
+    name1, name2 = edge
+    for substring, color in color_dict.items():
+        if substring in str(name1) and substring in str(name2):
+            return color
+        if "rail" in str(name1) or "rail" in str(name2):
+            if substring in str(name1) or substring in str(name2):
+                return color
+    # Default color if no match found
+    return "black"
+
+
+node_colors = [find_color_node(node) for node in graph1.nodes()]
+edge_colors = [find_color_edge(edge) for edge in graph1.edges()]
+
+networkx.draw_networkx(
+    graph1, pos, node_color=node_colors, edge_color=edge_colors, with_labels=True
+)
 plt.show()
